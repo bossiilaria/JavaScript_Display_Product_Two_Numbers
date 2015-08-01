1e0b941a41136b6be41d099f33d2346527d21b53
bossiilaria <odietamo.bossi200@gmail.com>
2015-08-01 17:41:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+	/*Write a program that asks the user for 2 numbers*/
+	
+	var num1 = window.prompt("Enter the first number");
+	var num2 = window.prompt("Enter the second number");
+	
+	var num3 = num1 * num2;
+	/*
+	window.prompt("The product of your numbers is " + num1 * num2");
+
+	*/
